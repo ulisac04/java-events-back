e2e7a20426b43f5887c989cb7f263e40aee9e032
Isaac Ramirez <ulisac04@gmail.com>
2025-12-10 11:24:06 -0400

    refactor security configuration to streamline request authorization and enable basic authentication

--- src/main/java/com/isaacjava/demoapi/config/SecurityConfig.java
+++ src/main/java/com/isaacjava/demoapi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,10 +33,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                ); // httpBasic desactivado para usar solo login via body
+                .authorizeHttpRequests( auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
