3cffa7270e5643ffaeade041d5b70b46fc4055cf
Isaac Ramirez <ulisac04@gmail.com>
2025-12-12 09:04:09 -0400

    add JwtAuthEntryPoint for handling unauthorized access; update SecurityConfig to integrate JWT authentication filter and exception handling

--- src/main/java/com/isaacjava/demoapi/config/SecurityConfig.java
+++ src/main/java/com/isaacjava/demoapi/config/SecurityConfig.java
@@ -1,21 +1,25 @@
 package com.isaacjava.demoapi.config;
 
+import com.isaacjava.demoapi.config.jwt.JwtAuthEntryPoint;
+import com.isaacjava.demoapi.config.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,8 +37,18 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests( auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated()
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+                ))
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/api/v1/auth/**")
+                        .permitAll()
+                     //   .requestMatchers(HttpMethod.GET, "/api/v1/events/**").permitAll()
+                       .anyRequest()
+                        .authenticated()
         ).httpBasic(Customizer.withDefaults());
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
